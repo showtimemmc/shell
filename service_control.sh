4c58424ffa2881140732f6137e371a233e165b5f
showtimemmc <showtimemmc@hotmail.com>
2014-04-02 00:22:13 +0800

    first

---
+++ service_control.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+SERVICES=("lighttpd" "mysql" "nmqproxy" "pusher" "topic" "nginx")
+function check_service()
+{
+	local len=$(ps -ef | grep $1 | egrep -v "grep|statusdetect.sh" | wc -l)
+	if [ $len == 0 ]; then
+		echo "$1 doesn't start"
+	else
+		echo "$1 started"
+	fi
+}
+#function start_service()
+#function stop_service()
+function check_all()
+{
+	for ser in ${SERVICES[@]}; 
+	do
+		check_service $ser
+	done
+}
+#function start_all()
+#function stop_all()
+function help()
+{
+	echo "usage{check|start|stop}"
+}
+#主程序入口
+if [ -n $1 ]; then
+	case $1 in
+		"check" )
+			check_all
+			;;
+		"lighttpd")
+			check_service lighttpd
+			;;	
+		*)
+			help
+			;;
+	esac
+fi
