e318fbe8cd18f0b1940ebe26c21d4d3af77e7148
showtimemmc <showtimemmc@hotmail.com>
2014-04-12 00:00:09 +0800

    bugfix

--- dirwithdate.sh
+++ dirwithdate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #创建文件夹以当前日期结尾
-if [[ -z $1 ]]; then
+if [ -z $1 ]; then
 	echo "please input a directory name"
+	exit 1
 fi
 DIRNAME=$1
 DATE=$(date +%y%m%d)
 FULLNAME=${DIRNAME}.${DATE}
 mkdir $FULLNAME
+exit 0
