5c4f2fb52c800347096a82f7b9cee1ce637b7f30
showtimemmc <showtimemmc@hotmail.com>
2014-04-12 22:28:47 +0800

    more control function

--- service_control.sh
+++ service_control.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SERVICES=("lighttpd" "mysql" "nmqproxy" "pusher" "topic" "nginx")
+#检查某一个服务是否启动
 function check_service()
 {
 	local len=$(ps -ef | grep $1 | egrep -v "grep|statusdetect.sh" | wc -l)
@@ -15,7 +16,33 @@ function nmqproxy_control()
 	echo $1
 	sh nmqproxy_control.sh $1
 }
-#function stop_service()
+function pusher_control()
+{
+	cd /home/work/nmq/pusher/bin
+	echo $1
+	sh pusher_control.sh $1
+}
+function topic_control()
+{
+	cd /home/work/nmq/topic/bin
+	echo $1
+	sh topic_control.sh $1
+}
+function nginx_control()
+{
+	cd /home/work/nginx/bin/
+	if [ $1 == "start" ]; then
+		./nginx
+	else
+		./nginx -s stop
+	fi
+	
+}
+function lighttpd_control()
+{
+	cd /home/work/orp/webserver/bin
+	sh lighttpd.sh $1
+}
 function check_all()
 {
 	for ser in ${SERVICES[@]}; 
@@ -27,7 +54,8 @@ function check_all()
 #function stop_all()
 function help()
 {
-	echo "usage{check|start|stop}"
+	echo  "usage{check|start|stop}\n
+			start:"
 }
 #主程序入口
 if [ -n $1 ]; then
