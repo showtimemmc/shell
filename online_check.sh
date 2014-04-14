ec8313cb102fd42f08d8f7891a1d79fcde9be97b
showtimemmc <showtimemmc@hotmail.com>
2014-04-14 17:31:52 +0800

    check nginx conf

--- online_check.sh
+++ online_check.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-#TODO search DB 
-#pull data from  orpxxx
-#HOME=export $HOME
-echo $HOME
-cd $HOME
-sed -n '1,10p' .vimrc
+#TODO:支持多中配置文件的抓取
+#TODO:抓取行数参数输入
+vars=`mysql -uroot orp << EOF
+select orpId from runtime;
+EOF`
+#vars中取出containerId，并将字段名containerId去掉
+orpIds=`echo $vars | awk -F '' '{print $0}' | sed 's/orpId//g'`
+for orpId in $orpIds
+do
+    echo $orpId
+#XXX:组成ORP目录，目前不支持orpId大于9
+    orpdir=orp00$orpId
+    echo $orp
+    sed -n '1,5p' $HOME/$orpdir/webserver/conf/nginx.conf
+done
+
