431130613fe1d804e5d3e1907e277319b8ddf9ec
showtimemmc <showtimemmc@hotmail.com>
2014-04-22 14:02:53 +0800

    add php control

--- service_control.sh
+++ service_control.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-SERVICES=("lighttpd" "mysql" "nmqproxy" "pusher" "topic" "nginx")
+SERVICES=("lighttpd" "mysql" "nmqproxy" "pusher" "topic" "nginx" "php")
+CONTROL_SCRIPTS=("lighttpd_control" "mysql_control" "nmqproxy_control" "pusher_control" "topic_control" "php_control")
 #检查某一个服务是否启动
 function check_service()
 {
@@ -10,24 +11,28 @@ function check_service()
 		echo "$1 started"
 	fi
 }
+#params: start|stop
 function nmqproxy_control()
 {
 	cd /home/work/nmq/nmqproxy/bin
 	echo $1
 	sh nmqproxy_control.sh $1
 }
+#params: start|stop
 function pusher_control()
 {
 	cd /home/work/nmq/pusher/bin
 	echo $1
 	sh pusher_control.sh $1
 }
+#params: start|stop
 function topic_control()
 {
 	cd /home/work/nmq/topic/bin
 	echo $1
 	sh topic_control.sh $1
 }
+#params: start|stop
 function nginx_control()
 {
 	cd /home/work/nginx/bin/
@@ -38,10 +43,17 @@ function nginx_control()
 	fi
 	
 }
+#params: start|stop
 function lighttpd_control()
 {
 	cd /home/work/orp/webserver/bin
 	sh lighttpd.sh $1
+}
+#params: start|stop
+function php_control()
+{
+	cd /home/work/orp/php/phplib/sbin
+	sh php-fpm start
 }
 function check_all()
 {
@@ -50,24 +62,30 @@ function check_all()
 		check_service $ser
 	done
 }
-#function start_all()
-#function stop_all()
+#params:start|stop
+function control_all()
+{
+	for ctrl in ${CONTROL_SCRIPTS[@]};
+	do
+		$ctrl $1
+	done
+}
+
 function help()
 {
-	echo  "usage{check|start|stop}\n
-			start:"
+	echo   "usage{check|start|stop}\n"
 }
 #主程序入口
-if [ -n $1 ]; then
+if [[ -n $1 ]]; then
 	case $1 in
 		"check" )
 			check_all
 			;;
-		"lighttpd")
-			check_service lighttpd
-			;;
 		"start")
-			nmqproxy_control   starting
+			control_all start
+			;;
+		"stop")
+			control_all stop
 			;;	
 		*)
 			help
